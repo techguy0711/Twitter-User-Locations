2c35c63524653d285a2bbc808eef7f9f2ab0dd77
kristhian <kristhian.deoliveira@me.com>
2021-04-08 12:42:06 -0400

    Initial Commit

---
+++ Twitter User Locations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twitter User Locations
+//
+//  Created by Kristhian De Oliveira on 4/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
